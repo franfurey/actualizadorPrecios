e8ff29d7025e68062e60b381902f2b46d16592fe
Francisco Furey <franciscofurey@gmail.com>
2023-08-04 19:32:03 -0300

    add gunicorn

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
-from dotenv import load_dotenv
-import os
-
-# Cargar las variables de entorno del archivo .env
-load_dotenv('db.env')
-
-# Obtener el valor de la variable DATABASE_URL
-database_url = os.getenv("DATABASE_URL")
-
-# Imprimir el valor de la variable DATABASE_URL
-print(database_url)
+import numpy
+print(numpy.__version__)
