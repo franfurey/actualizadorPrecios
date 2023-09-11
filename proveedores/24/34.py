a2c25a3f25a777bfca400d256bf03645aedb3c2d
Francisco Furey <franciscofurey@gmail.com>
2023-09-11 17:02:16 -0300

    Adding new client!

--- proveedores/24/34.py
+++ proveedores/24/34.py
@@ -1,1 +1,60 @@
 # Este es el archivo del proveedor: Drimel
+import re
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+def process_proveedor_file(urls):
+    session = requests.Session()
+
+    marcas = ['Algabo', 'Duffy', 'Huggies','Babysec','Caricia','Estrella','Pampers',
+              'Candy','Doncella','Deyse','Johnsons','Kimbies','Upa']
+
+    df = pd.DataFrame(columns=['name', 'price','marca','product_id', 'product_sku','url_id','MPN (Número de pieza del fabricante)'])        
+    
+    for extract_url in urls: 
+        page = 1
+        while True:
+            url = extract_url + '&paged={}'.format(page)
+
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            products = soup.find_all('div', class_='box-text box-text-products')
+            if not products:
+                break
+
+            for product in products:
+                name = product.find('a', class_='woocommerce-LoopProduct-link').text.strip()
+                price = product.find('span', class_='woocommerce-Price-amount').text.strip()[1:]
+                price = price.replace('"', '').replace('.', '').split(',')[0]
+                
+                marca = ''
+                for m in marcas:
+                    if m.lower() in name.lower():
+                        marca = m
+                        break
+
+                add_to_cart_button = product.find('a', class_='add_to_cart_button')
+                product_id = add_to_cart_button.get('data-product_id', '')
+                product_sku = add_to_cart_button.get('data-product_sku', '')
+                url_id = re.sub('[^a-zA-Z0-9]+', '-', name).strip('-')
+                url_id = url_id.replace(' ', '-')
+                
+                df = df[(df['marca'] != 'Algabo') & (df['marca'] != 'Upa')]
+
+                MPN = 'Drimel'
+                df.loc[len(df)] = [name, price, marca, product_id, product_sku, url_id, MPN]
+            page += 1
+
+    df = df.rename(columns={'name': 'Nombre', 'price': 'Costo',
+                             'product_id': 'SKU', 'product_sku': 'Código de barras',
+                               'url_id':'Identificador de URL', 'marca':'Marca'})
+    
+    df['Costo'] = pd.to_numeric(df['Costo'])
+    df['Costo'] = df['Costo'].apply(lambda x: round(x*0.9))
+
+    df['SKU'] = df['SKU'].astype(int)
+    df = df.sort_values('SKU', ascending=True)
+
+    return df
--- proveedores/24/35.py
+++ proveedores/24/35.py
@@ -1,1 +1,35 @@
 # Este es el archivo del proveedor: Teddy
+import pandas as pd
+
+def process_proveedor_file(df):
+    """Filtrar y reformatear un DataFrame de Excel para Teddy"""
+    df = df.drop(index=range(19))
+    df = df.rename(columns={
+        df.columns[1]: 'SKU',
+        df.columns[2]: 'Código de barras',
+        df.columns[3]: 'Nombre',
+        df.columns[4]: 'Costo'
+    })
+    df = df[['SKU', 'Código de barras', 'Nombre', 'Costo']]
+
+    # Continua el proceso de limpieza...
+    # ...
+    # Eliminar filas sin un código asociado
+    columna = df.iloc[:, 1]
+    df = df.dropna(subset=[df.columns[1]])
+    df['Código de barras'] = df['Código de barras'].astype(int).astype(str)
+    df['Código de barras'] = df['Código de barras'].str.replace(r'[.+\-E]', '', regex=True)
+    df['Código de barras'] = df['Código de barras'].str.zfill(13)
+
+    # Convertir la columna SKU_numero a números
+    df["SKU"] = pd.to_numeric(df["SKU"], errors="coerce")
+
+    # Ordenar por SKU_numero de forma ascendente
+    df = df.sort_values(by='SKU')
+
+
+    df['Costo'] = df['Costo'].replace(',', '', regex=True)
+
+    # Limpiar los valores de la columna "Costo"
+    df['Costo'] = df['Costo'].fillna(0).apply(lambda x: int(float(str(x).split('.')[0])))
+    return df
