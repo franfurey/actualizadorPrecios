36b593507525075059264543230b0813232a84cd
Francisco Furey <franciscofurey@gmail.com>
2023-12-13 08:59:30 -0300

    change number of workers

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ service nginx start
 
 # Calculate the number of cores
 NUM_CORES=$(nproc --all)
-# Calculate the number of workers based on the number of cores
-WORKERS=$((NUM_CORES * 2 + 1))
+# Set a lower number of workers for a t2.micro instance
+WORKERS=$((NUM_CORES + 1))
 
-# Start Gunicorn on port 8000 with debug logging and multiple workers with gevent
+# Start Gunicorn with the specified number of workers
 gunicorn -b 0.0.0.0:8000 --log-level debug --workers $WORKERS --worker-class gevent main:app
